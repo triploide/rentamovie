6bbd79c431bfbb9ecec9ace2f214fb79c6f6fd44
Maxi Yañez <aeroventila@gmail.com>
2017-06-11 23:23:57 -0300

    Estrategia para el AMB en base de datos JSON

--- php/classes/JSONDB.php
+++ php/classes/JSONDB.php
@@ -7,35 +7,70 @@ class JSONDB {
         $registros = file_get_contents('../../usuarios.json');
         $registros = (!$registros)?[]:json_decode($registros, true);
         foreach ($registros[$table] as $registro) {
-           if ($registro['id'] == $id) {
+            if ($registro['id'] == $id) {
                 $model = new $class([]);
                 $model->toModel($registro);
                 return $model;
-           }
+            }
         }
         return null;
     }
 
     public function save($table, $model)
     {
-        //To do
+        //pregunto si el modelo tiene un id
+        if ($model->id) {
+            //si lo tiene, es decir que ya existe en la base, por tanto lo actualizo
+            $this->update($table, $model);
+        } else {
+            //ni no, creo uno nuevo
+            $this->insert($table, $model);
+        }
     }
 
     public function insert($table, $model)
     {
-        $columns = implode(', ', $model->fillable);
-        $placeholders = ':'.implode(', :', $model->fillable);
-        return "INSERT INTO ".$table." ($columns) VALUES ($placeholders)";
+        $registros = file_get_contents('../../usuarios.json');
+        $registros = (!$registros)?[]:json_decode($registros, true);
+        $nuevoRegistro = [];
+        foreach ($model->fillable as $column) {
+            $nuevoRegistro[$column] = $model->$column;
+        }
+        //uso esto para emular el comportamiento que tiene mysql al generar ids
+        //con uniqid garantizo que los ids de los registros no se repitan, aunque sacrifico el autoincrement
+        $nuevoRegistro['id'] = uniqid();
+        $registros[$table][] = $nuevoRegistro;
+        $registros = json_encode($registros);
+        file_put_contents('../../usuarios.json', $registros);
     }
 
     public function update($table, $model)
     {
-        $set = '';
-        foreach ($model->fillable as $column) {
-            $set .= "$column=:$column,";
+        //Busco en la base el objeto que tengo que actualizar, utilizo la misma lógica que find
+        $registros = file_get_contents('../../usuarios.json');
+        $registros = (!$registros)?[]:json_decode($registros, true);
+        //uso el for en vez del foreach para trabajar siempre con el array completo del json. Porque en el foreach recupero
+        //el $registro como una variable nueva, independiente de $registros (plural). Eso hace que modificar $registro no tenga
+        //incidencia en el array general. Es decir $registro['nombre'] = 'Nuevo nombre' no termina repercutiendo en $registros.
+        //Pero si lo haría esto: $registros[4]['nombre'] = 'Nuevo nombre'
+        for ($i=0, $l=count($registros[$table]); $i<$l; $i++) {
+            //Si el registro actual tiene el mismo id que el modelo con el que estoy trabajando:
+            //eso significa que ese va a ser el registro que voy a modificar
+            if ($registros[$table][$i]['id'] == $model->id) {
+                //ahora que encontré el registro que tengo que modificar en el json, seteo cada uno de sus propiedades
+                //con los valores que tiene el modelo. Para saber que propiedes debo modificar me valgo del atributo $fillable
+                foreach ($model->fillable as $column) {
+                    $registros[$table][$i][$column] = $model->$column;
+                }
+                //finalmente guardo el registro
+                //lo codifico a json
+                $registros = json_encode($registros);
+                //guardarlo en el archivo json
+                file_put_contents('../../usuarios.json', $registros);
+                //corto el bucle con el break
+                break;
+            }
         }
-        $set = trim($set, ",");
-        return "UPDATE ".$table." SET $set WHERE id = ".$model->id;
     }
 
 }
--- php/test/usuario.test.php
+++ php/test/usuario.test.php
@@ -7,13 +7,16 @@ require_once '../classes/Pelicula.php';
 DBFactory::$db_type = 'JSONDB';
 
 /*
-$usuario = new Usuario('Bart2', 'bart2@mail.com', '123', 10);
-$usuario->save();
+$pelicula = new Pelicula(['titulo' => 'Los hombres son golpeados por el Fútbol', 'estreno' => '1994-03-09', 'genero_id' => 3]);
+$pelicula->save();
+exit;
 */
 
-$usuario = Usuario::find(2);
-var_dump($usuario); exit;
-$pelicula->titulo = 'Avatar';
+/*
+$pelicula = Pelicula::find(5);
+$pelicula->titulo = 'Avatar 3';
 $pelicula->save();
+*/
 
-
+$pelicula = Pelicula::find('593dfab1a9b07');
+var_dump($pelicula);
